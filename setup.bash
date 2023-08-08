022efd7907e2d9c9f1b970672a004c44538219ba
Trent Weiss <ttw2xk@virginia.edu>
2023-08-08 10:56:37 -0400

    suppress tensorflow warnings

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 scriptpath=$(cd $(dirname "${BASH_SOURCE:-$0}") && pwd)
 export PYTHONPATH=${scriptpath}:${PYTHONPATH}
-pyversion=$(python3 --version | grep -E "3.([0-9])*" -o | tr -d ".")
+pyversion=$(python3 -c "import sys; print(str(sys.version_info.major)+str(sys.version_info.minor))")
 extraldpaths=${scriptpath}/build/lib.linux-x86_64-cpython-${pyversion}/mtr/ops/attention:${scriptpath}/build/lib.linux-x86_64-cpython-${pyversion}/mtr/ops/knn
 if [ -z ${LD_LIBRARY_PATH} ]
 then
@@ -9,3 +9,4 @@ then
 else
     export LD_LIBRARY_PATH=${extraldpaths}:${LD_LIBRARY_PATH}
 fi
+export TF_CPP_MIN_LOG_LEVEL=2
